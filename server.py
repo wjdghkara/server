908ee18a7c6308af0800b8bdd85c30821955881b
wjdghkara <39269019+wjdghkara@users.noreply.github.com>
2018-07-20 12:27:30 +0900

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+# server
+
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0',12345))
+server_socket.listen(0)
+client_socket, addr = server_socket.accept()
+data = client_socket.recv(65535)
+
+print(data.decode())
+
+client_socket.send(data)
